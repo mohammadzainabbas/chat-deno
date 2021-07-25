f9f7679c38d93892b4dc77d8699213995d9b2362
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2021-07-25 19:22:47 +0500

    Updated

---
+++ src/utils/chat.ts
@@ -0,0 +1,35 @@
+import { WebSocket, isWebSocketCloseEvent } from 'https://deno.land/std/ws/mod.ts';
+import { v4 } from 'https://deno.land/std/uuid/mod.ts';
+import { camelize } from './camelize';
+
+const users = new Map<string, WebSocket>();
+
+const broadcast = (message: string, senderId?: string): void => {
+	if (!message) return;
+
+	for (const user of users.values()) {
+		user.send(senderId ? `[${senderId}] : ${message}` : message);
+	}
+};
+
+export const chat = (ws: WebSocket): Promise<void> => {
+	const userId = v4?.generate();
+
+	users.set(userId, ws);
+
+	broadcast(`> User '${userId}' is connected ✅`);
+
+	for await (const event of ws) {
+		const message = camelize(typeof event === 'string' ? event : '');
+
+		broadcast(message, userId);
+
+		if (!message && isWebSocketCloseEvent(event)) {
+			users.delete(userId);
+
+			broadcast(`> User '${userId}' is disconnected ❌`);
+
+			break;
+		}
+	}
+};
